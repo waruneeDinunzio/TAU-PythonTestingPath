4b5e165b01127d8cf430a3a2211f7fb27155ae0f
warunee dinunzio <dinunziow@gmail.com>
2024-12-04 12:05:41 -0500

    Add Introduction to Pytest

---
+++ IntroToPytest/tests/test_accum.py
@@ -0,0 +1,54 @@
+"""
+This module contains basic unit tests for the accum module.
+Their purpose is to show how to use the pytest framework by example.
+"""
+
+# --------------------------------------------------------------------------------
+# Imports
+# --------------------------------------------------------------------------------
+
+import pytest
+from stuff.accum import Accumulator
+
+
+# --------------------------------------------------------------------------------
+# Fixtures
+# --------------------------------------------------------------------------------
+
+@pytest.fixture
+def accum():
+  return Accumulator()
+
+
+# --------------------------------------------------------------------------------
+# Tests
+# --------------------------------------------------------------------------------
+
+@pytest.mark.accumulator
+def test_accumulator_init(accum):
+  assert accum.count == 0
+
+
+@pytest.mark.accumulator
+def test_accumulator_add_one(accum):
+  accum.add()
+  assert accum.count == 1
+
+
+@pytest.mark.accumulator
+def test_accumulator_add_three(accum):
+  accum.add(3)
+  assert accum.count == 3
+
+
+@pytest.mark.accumulator
+def test_accumulator_add_twice(accum):
+  accum.add()
+  accum.add()
+  assert accum.count == 2
+
+
+@pytest.mark.accumulator
+def test_accumulator_cannot_set_count_directly(accum):
+  with pytest.raises(AttributeError, match=r"property 'count' of 'Accumulator' object has no setter") as e:
+    accum.count = 10
